7eef267fc4e4ef6e465c862af4bc3c0a1ae9ec58
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2022-12-02 16:15:08 +0100

    Day2

--- AoC day 2/rust/solution_second_part.rs
+++ AoC day 2/rust/solution_second_part.rs
@@ -1,5 +1,42 @@
-use std::fs;
+#!/usr/bin/env rust-script
 
 fn main() {
-	println!("Hello, world!");
+	let input = std::fs::read_to_string("input").unwrap();
+
+	let res = input
+		.split_terminator("\n") // per evitare l'ultima riga vuota
+		.map(|line| {
+			let mut split = line.split(' ');
+			
+			( split.next().unwrap(), split.next().unwrap() )
+		})
+		.map(|(first, second)| {
+			( 
+				first.chars().next().unwrap() as u8 - 'A' as u8, 
+				second.chars().next().unwrap() as u8 - 'X' as u8
+			)
+		})
+		.map(|(first, second)| {
+			(
+				first, 
+				(first + second + 2) % 3
+			)
+		})
+		.map(|(first, second)| {
+			let play_diff = (3 + second - first) % 3; // +3 per evitare numeri negativi
+			
+			let won_pts = match play_diff {
+				0 => 3,
+				1 => 6,
+				2 => 0,
+				_ => panic!("Invalid play diff"),
+			};
+
+			let play_pts = second + 1;
+
+			(won_pts + play_pts) as u32
+		})
+		.sum::<u32>();
+		
+	println!("{res}");
 }
