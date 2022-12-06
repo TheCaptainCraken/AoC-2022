537a1b21b7c2aec1e0fccbadae92f90925b254f4
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2022-12-06 14:11:00 +0100

    Day6

--- AoC day 6/c/solution_first_part.c
+++ AoC day 6/c/solution_first_part.c
@@ -1,7 +1,54 @@
+//usr/bin/gcc solution_first_part.c && exec ./a.out
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
+int is_unique_str(char *str) {
+	for (size_t i = 0; str[i]; i++)
+		for (size_t j = i + 1; str[j]; j++)
+			if (str[i] == str[j])
+				return 0;
+
+	return 1;
+}
+
 int main(int argc, char** argv) {
+	FILE *fp = NULL;
+
+	fp = fopen("input", "r");
+
+	if (fp == NULL)
+		return 1;
+
+	fseek(fp, 0, SEEK_END);
+
+	size_t size = ftell(fp);
+
+	fseek(fp, 0, SEEK_SET);
+
+	char *buffer = calloc(size + 1, sizeof (char));
+	char *prev_four = buffer;
+	size_t i = 0;
+
+	for (; i < 4; i++, buffer++)
+		*buffer = fgetc(fp);
+
+	while (!feof(fp)) {
+		*buffer = fgetc(fp);
+
+		++buffer;
+		++prev_four;
+		++i;
+
+		if (is_unique_str(prev_four)) {
+			printf("%d\n", i);
+			break;	
+		}
+	}
+
+	fclose(fp);
+
 	return 0;
 }
--- AoC day 6/rust/solution_second_part.rs
+++ AoC day 6/rust/solution_second_part.rs
@@ -1,5 +1,16 @@
-use std::fs;
+#!/usr/bin/env -S rust-script
+
+use std::collections::HashSet;
 
 fn main() {
-	println!("Hello, world!");
+	let input = std::fs::read_to_string("input").unwrap().chars().collect::<Vec<_>>();
+
+	let position = input
+		.windows(14)
+		.position(|window| {
+			window.iter().collect::<HashSet<_>>().len() == window.len()
+		})
+		.unwrap();
+
+	println!("{}", position + 14);
 }
